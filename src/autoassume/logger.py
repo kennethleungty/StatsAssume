93414fb6994423023b42b3abda8aba8c6ff3dda0
Kenneth Leung <kennethleung.ty@gmail.com>
2022-01-30 09:57:42 +0800

    Update logger.py

--- src/autoassume/logger.py
+++ src/autoassume/logger.py
@@ -1,14 +1,14 @@
 # ==========================
 # Module: Logging
 # Author: Kenneth Leung
-# Last Modified: 12 Jan 2022
+# Last Modified: 30 Jan 2022
 # ==========================
 
 try:
     from loguru import logger
     logger.remove()  # Remove default 'stderr' sink (and all others, if any)
     # logger.add('logs/autoassume_check.log', mode="w")
-    logger.add("logs/my_log_file.log", rotation="10 MB", mode="w")
+    logger.add("logs/autoassume_check.log", rotation="10 MB", mode="w")
 except ImportError:
     import logging
     handler = logging.StreamHandler()
