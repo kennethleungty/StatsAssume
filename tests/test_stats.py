07e02a3e5ec6f402c62ef73d46f4d775d0781715
Kenneth Leung <kennethleung.ty@gmail.com>
2022-02-04 23:06:25 +0800

    Update Actions workflow

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -1,7 +1,15 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../src"))
+# sys.path.insert(0, os.path.abspath(".."))
 
-import pyassume.stats
+# import pyassume.stats
 
+def test_sample_456():
+    var1 = 1
+    var2 = 2
+    var3 = 3
+
+    assert (var1 + var2) == var3
+
+# def test_stat_durbin()
