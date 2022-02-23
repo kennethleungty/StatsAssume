1c264b85f23dd3c65b91027e7fd8d0c6f3ffc85c
Kenneth Leung <kennethleung.ty@gmail.com>
2022-02-23 20:20:04 +0800

    Update test_stats.py

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -3,16 +3,13 @@ import sys
 
 sys.path.insert(0, os.path.abspath(".."))
 
-from pyassume.datasets import load_data
+# from checkassume.datasets import load_data
 
+# def test_stat_breuschpagan():
+#     var1 = 1
+#     var2 = 2
+#     var3 = 3
 
-
-
-def test_stat_breuschpagan():
-    var1 = 1
-    var2 = 2
-    var3 = 3
-
-    assert (var1 + var2) == var3
+#     assert (var1 + var2) == var3
 
 # def test_stat_durbin()
